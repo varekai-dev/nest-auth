f3b0c72e437ca36b555519634dfc057e528fd8f1
Serhii <s.saviuk@apiko.com>
2023-11-30 00:50:54 +0200

    implement refresh token

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -12,9 +12,10 @@ import { SignInDto } from './dto/sign-in.dto';
 import { Response } from 'express';
 import { Auth } from './decorators/auth.decorator';
 import { AuthType } from './enums/auth-type.enum';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 
 @Auth(AuthType.None)
-@Controller('authentication')
+@Controller('auth')
 export class AuthenticationController {
   constructor(private readonly authService: AuthenticationService) {}
   @Post('sign-up')
@@ -28,12 +29,15 @@ export class AuthenticationController {
     @Res({ passthrough: true }) response: Response,
     @Body() signInDto: SignInDto,
   ) {
-    const accessToken = await this.authService.signIn(signInDto);
-    response.cookie('accessToken', accessToken, {
-      secure: true,
-      httpOnly: true,
-      sameSite: true,
-    });
-    return accessToken;
+    return await this.authService.signIn(signInDto, response);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('refresh-token')
+  async refreshTokens(
+    @Res({ passthrough: true }) response: Response,
+    @Body() refreshTokenDto: RefreshTokenDto,
+  ) {
+    return await this.authService.refreshTokens(refreshTokenDto, response);
   }
 }
--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -14,9 +14,16 @@ import { JwtService } from '@nestjs/jwt';
 import { ConfigType } from '@nestjs/config';
 import jwtConfig from 'iam/config/jwt.config';
 import { ActiveUserData } from 'iam/interfaces/active-user-data';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthenticationService {
+  private readonly jwtConfig = {
+    secret: this.jwtConfiguration.secret,
+    audience: this.jwtConfiguration.audience,
+    issuer: this.jwtConfiguration.issuer,
+  };
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
@@ -39,7 +46,7 @@ export class AuthenticationService {
       throw error;
     }
   }
-  async signIn(signInDto: SignInDto) {
+  async signIn(signInDto: SignInDto, response: Response) {
     const user = await this.userRepository.findOneBy({
       email: signInDto.email,
     });
@@ -53,26 +60,71 @@ export class AuthenticationService {
     if (!isEqual) {
       throw new UnauthorizedException('Password is incorrect');
     }
-    const accessToken = await this.generateJwtToken(user);
+    const tokens = await this.generateTokens(user);
+
+    this.setCookies(response, tokens);
+
+    return tokens;
+  }
+
+  async generateTokens(user: User) {
+    const [accessToken, refreshToken] = await Promise.all([
+      this.signToken<Partial<ActiveUserData>>(
+        user.id,
+        this.jwtConfiguration.accessTokenTtl,
+        {
+          email: user.email,
+        },
+      ),
+      this.signToken(user.id, this.jwtConfiguration.refreshTokenTtl),
+    ]);
 
     return {
       accessToken,
+      refreshToken,
     };
   }
 
-  private async generateJwtToken(user: User) {
-    const payload = {
-      sub: user.id,
-      email: user.email,
-    } satisfies ActiveUserData;
+  async refreshTokens(refreshTokenDto: RefreshTokenDto, response: Response) {
+    try {
+      const { sub } = await this.jwtService.verifyAsync<
+        Pick<ActiveUserData, 'sub'>
+      >(refreshTokenDto.refreshToken, this.jwtConfig);
 
+      const user = await this.userRepository.findOneByOrFail({
+        id: sub,
+      });
+      const tokens = await this.generateTokens(user);
+
+      this.setCookies(response, tokens);
+
+      return tokens;
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
+  }
+
+  private setCookies(response: Response, tokens: Record<string, string>) {
+    // Set response cookies with refresh and access tokens
+    Object.keys(tokens).forEach((key) => {
+      response.cookie(key, tokens[key], {
+        secure: true,
+        httpOnly: true,
+        sameSite: true,
+      });
+    });
+  }
+
+  private async signToken<T>(userId: number, expiresIn: number, payload?: T) {
+    const data = {
+      sub: userId,
+      ...payload,
+    };
     const config = {
-      secret: this.jwtConfiguration.secret,
-      audience: this.jwtConfiguration.audience,
-      issuer: this.jwtConfiguration.issuer,
-      expiresIn: this.jwtConfiguration.accessTokenTtl,
+      ...this.jwtConfig,
+      expiresIn,
     };
 
-    return await this.jwtService.signAsync(payload, config);
+    return await this.jwtService.signAsync(data, config);
   }
 }
